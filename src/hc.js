4733109e42a142122b3b9f028ce1fce6a2a3f992
JhonatanHern <jhonatanhernandez998@gmail.com>
2019-08-28 11:11:15 -0400

    external url bug fixed

--- src/hc.js
+++ src/hc.js
@@ -1,7 +1,7 @@
 import { connect } from '@holochain/hc-web-client'
 
 export default ({ functionName , params = {} , callback = _ => {} }) => {
-    let promise = connect("ws://35.239.195.221:8888")
+    let promise = connect({ url: "ws://35.239.195.221:8888" })
     promise.then(({callZome, close}) => {
         callZome( 'test-instance' , 'messaging' , functionName )( params ).then((result) => {
             result = JSON.parse(result)
